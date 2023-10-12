022068e0d06a3cc8e37dedd67751c40eeeb97cd5
Margaret-00 <marggaretmaina566@gmail.com>
2023-10-12 13:05:41 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+char ch;
+for (ch m 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
