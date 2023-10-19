75b4aaf41d1eac62f1dfe03d70fab1bc091151d3
Margaret-00 <marggaretmaina566@gmail.com>
2023-10-19 14:20:24 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include <main.h>
+/**
+ * _strncpy - function that cpies a string, with number of bytes
+ * @dest: Destination output
+ * @src: source value
+ * @n: number of bytes
+ *
+ * Return: copied strings with number of bytes
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; index < n && src[index] != '\0' ; index++)
+	{
+		dest[index] = src[index];
+	}
+	for (; index  < n; index++)
+	{
+		dest[index] = '\0';
+	}
+	return (dest);
+}
