1f3292aa7ee9688bd17dbe089135f121b338a7c8
Margaret-00 <marggaretmaina566@gmail.com>
2023-10-19 16:43:59 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
-	if (n <0)
+	if (n < 0)
 	{
 	_putchar('_');
 	num = -num;
@@ -19,6 +19,6 @@ void print_number(int n)
 
 	if ((num / 10) > 0)
 	print_number(num / 10);
-	_prtchar((num % 10) + '0');
+	_putchar((num % 10) + '0');
 
 }
