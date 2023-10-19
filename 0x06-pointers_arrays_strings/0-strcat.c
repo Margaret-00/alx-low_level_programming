f22964329e10a71e3f8caa7b74d28196fc7c6695
Margaret-00 <marggaretmaina566@gmail.com>
2023-10-19 12:48:07 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,16 @@
 char *_strcat(char *dest, char *src);
 {
 
-int i, j
+int a=-1, i;
 
-	for (i = 0; dest[i] != '\0'; i++);
+	for (i = 0; dest[i]!= '\0'; i++);
 		
-	for (j = 0; src[j] != '\0'; j++);
-	
-		dest[i] = src[j];
-		i++;
-	
-	return (dest);
+do{
+     a++;
+     dest[i]=src[a];
+     i++;
+} while(src[a] != '\0');
+
+
+return (dest);
 }
