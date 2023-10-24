54ecb0d237df1587bd38ea23f9cb49a5cbb5c853
Margaret-00 <marggaretmaina566@gmail.com>
2023-10-24 03:03:21 -0700

    memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,15 +7,18 @@
  * Return: void
  */
 
-char *_memset(char *s, char b, unsigned int n);
-{
-	int i = 0;
+char *_memset(char *s, char b, unsigned int n)
 
-	for (; n > 0; i++)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+
 	{
 		s[i] = b;
 
-		n--;
 	}
+
 	return (s);
+
 }
