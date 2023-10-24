c2f95cfe0f79dec801f7e1fefe5a9b375539dbae
Margaret-00 <marggaretmaina566@gmail.com>
2023-10-24 13:15:07 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,10 +12,16 @@ unsigned int _strspn(char *s, char *accept)
 
 	while (*s)
 	{
+
 		for (r = 0; accept[r]; r++)
 		{
-			n++;
-			break;
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
 		}
 		s++;
 	}
