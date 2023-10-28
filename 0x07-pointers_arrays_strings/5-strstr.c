a0566b2199f55ab9dacd68690956fc4aa5d7f30b
Margaret-00 <marggaretmaina566@gmail.com>
2023-10-27 20:28:31 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring within a string.
+ * @haystack: The string to search in.
+ * @needle: The substring to find.
+ * Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *temp;
+
+		temp = haystack;
+		while (*needle == *haystack)
+		{
+			haystack++, needle++;
+		}
+		if (*needle == '\0')
+			return (temp);
+		haystack++;
+	}
+	return (0);
+}
